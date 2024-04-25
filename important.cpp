ea51b6ca9c4f8e552902557925ef9403d3c1600f
kk9162 <khuship9162@gmail.com>
2024-04-24 18:49:32 -0700

    printing numbers

--- important.cpp
+++ important.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 
+int printNums() {
+     using namespace std;
+    for (int i = 0; i < 20; i++)
+    {
+        cout << "this is line number: " << i << endl;
+    }
+    return 0;
+}
+
 int main() {
     // Include the namespace std
     using namespace std;
     
     cout << "hello" << endl;
+    printNums();
 
     return 0;
 }
+
